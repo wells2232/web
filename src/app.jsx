d3f904072d9e699e28e9c80e66ba5f2d70a0d9ef
Welli <wellington.nunes10@hotmail.com>
2025-07-09 20:32:33 -0300

    feat: created register and login page

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthPage } from './pages/auth-page';
+import { HomePage } from './pages/home-page';
+
+const queryClienty = new QueryClient();
+
 export function App() {
-  return <div className="min-h-screen ">{}</div>;
+  return (
+    <QueryClientProvider client={queryClienty}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HomePage />} path="/" />
+          <Route element={<AuthPage />} path={'/auth'} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
---
+++ src/components/auth-form.jsx
@@ -0,0 +1,187 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import z from 'zod';
+import { useLogin, useRegister } from '../services/auth-service';
+
+const registerSchema = z.object({
+  name: z
+    .string()
+    .min(2, { error: 'Nome deve ter no mínimo 2 caracteres' })
+    .default(''),
+  email: z.email({ error: 'Por favor, insira um e-mail válido.' }).default(''),
+  password: z
+    .string()
+    .min(6, { error: 'A Senha deve contém ao menos 6 catacteres' })
+    .default(''),
+});
+
+const loginSchema = registerSchema.pick({
+  email: true,
+  password: true,
+});
+
+export function AuthForm() {
+  const [isLogin, setIsLogin] = useState(false);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+  const { mutateAsync: login } = useLogin();
+  const { mutateAsync: register } = useRegister();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+    setFormData({ email: '', password: '' });
+    setErrors({});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({});
+
+    const schema = isLogin ? loginSchema : registerSchema;
+    const result = schema.safeParse(formData);
+
+    if (!result.success) {
+      const prettyErrors = z.flattenError(result.error).fieldErrors;
+
+      setErrors(prettyErrors);
+    }
+
+    const validatedData = result.data;
+
+    // Sugestão
+    try {
+      if (isLogin) {
+        await login(validatedData);
+        navigate('/');
+      } else {
+        await register(validatedData);
+        navigate('/');
+      }
+    } catch (error) {
+      if (error.response?.data?.errors) {
+        // 1. Pega os erros estruturados da API
+        const backendErrors = error.response.data.errors;
+
+        setErrors(backendErrors);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full max-w-3/4 overflow-hidden rounded-xl shadow-2xl md:max-w-3/4 lg:max-w-2/3">
+      <div className="flex flex-col md:flex-row">
+        <div className=" w-full bg-orange-200 p-8 text-zinc-900 md:w-1/2 md:p-12">
+          <h2 className="mb-6 text-center font-bold text-2xl text-gray-800">
+            {isLogin ? 'Login' : 'Crie sua Conta'}
+          </h2>
+          <form
+            className="flex flex-col gap-4"
+            noValidate
+            onSubmit={handleSubmit}
+          >
+            {!isLogin && (
+              <div>
+                <label
+                  className="mb-2 block font-semibold text-sm"
+                  htmlFor="name"
+                >
+                  Nome
+                </label>
+                <input
+                  className="w-full rounded-md bg-white px-4 py-3 text-black transition focus:outline-none focus:ring-2 focus:ring-amber-600 "
+                  id="name"
+                  name="name"
+                  onChange={handleInputChange}
+                  placeholder="Seu nome"
+                  type="text"
+                  value={formData.name}
+                />
+                {errors.name && (
+                  <p className="mt-1 text-red-900 text-xs">{errors.name[0]}</p>
+                )}
+              </div>
+            )}
+
+            <div>
+              <label
+                className="mb-2 block font-semibold text-sm"
+                htmlFor="email"
+              >
+                E-mail
+              </label>
+              <input
+                className="w-full rounded-md bg-white px-4 py-3 text-black transition focus:outline-none focus:ring-2 focus:ring-amber-600 "
+                id="email"
+                name="email"
+                onChange={handleInputChange}
+                placeholder="email@exemplo.com"
+                required
+                type="email"
+                value={formData.email}
+              />
+              {errors.email && (
+                <p className="mt-1 text-red-900 text-xs">{errors.email[0]}</p>
+              )}
+            </div>
+            <div>
+              <label
+                className="mb-2 block font-semibold text-sm"
+                htmlFor="password"
+              >
+                Senha
+              </label>
+              <input
+                className="w-full rounded-md bg-white px-4 py-3 text-black transition focus:outline-none focus:ring-2 focus:ring-amber-600 "
+                id="password"
+                name="password"
+                onChange={handleInputChange}
+                placeholder="••••••••"
+                required
+                type="password"
+                value={formData.password}
+              />
+              {errors && (
+                <p className="mt-1 text-red-900 text-xs">{errors.password}</p>
+              )}
+            </div>
+
+            <button
+              className="w-full transform rounded-lg bg-amber-600 px-4 py-3 font-bold text-white transition-transform hover:scale-102 hover:cursor-pointer hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+              type="submit"
+            >
+              {isLogin ? 'Entrar' : 'Registrar'}
+            </button>
+            <div>
+              <p className="mt-2 text-center text-gray-600 text-sm">
+                {isLogin ? 'Não tem uma conta? ' : 'Já tem uma conta? '}
+                <button
+                  className="font-medium text-amber-500 hover:text-amber-700 "
+                  onClick={toggleForm}
+                  type="button"
+                >
+                  {isLogin ? 'Registre-se' : 'Faça login'}
+                </button>
+              </p>
+            </div>
+          </form>
+        </div>
+        <div className=" w-full bg-gradient-to-br from-indigo-500 to-emerald-500 md:w-1/2 md:p-12" />
+
+        <div />
+      </div>
+    </div>
+  );
+}
