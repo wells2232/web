40723e9da786e46b949d765313342babe7b16147
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2025-07-22 01:09:28 -0400

    modal inicial

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,21 +1,46 @@
-function Card({ nome, descricao, condicao, foto }) {
+import { BadgeInfo } from 'lucide-react';
+export default function ItemCard({ item_name, description, condition, image_url, onClick}) {
+
+  const iconColorClass =
+    condition === 'Novo'
+      ? 'text-green-600 '
+      : condition === 'Seminovo'
+      ? 'text-yellow-500'
+      : 'text-gray-600';
+
+  const badgeClass =
+    condition === 'Novo'
+      ? 'bg-green-300 text-green-700'
+      : condition === 'Seminovo'
+      ? 'bg-yellow-300 text-yellow-500'
+      : 'bg-gray-700 text-gray-700';
+
+
   return (
-    <div className="min-w-80 rounded-2xl border border-gray-200 bg-white p-4 shadow-md hover:shadow-laranja ">
-      <h2 className="font-bold text-gray-900 text-lg">{nome}</h2>
-      <p className="mb-1 text-gray-600 text-sm">{descricao}</p>
-      <span
-        className={`inline-block rounded-full px-2 py-1 text-xs ${
-          condicao === 'Novo'
-            ? 'bg-green-100 text-green-700'
-            : condicao === 'Seminovo'
-              ? 'bg-yellow-100 text-yellow-700'
-              : 'bg-gray-200 text-gray-700'
-        }`}
-      >
-        {condicao}
-      </span>
+    
+    <div className="bg-white min-w-60 max-w-100 flex flex-col min-h-100 rounded-2xl shadow-md p-4 border border-gray-200 relative">
+    <div className=" aspect-square  overflow-hidden rounded-t-xl ">
+      <img src={image_url} alt={item_name} className=" w-full h-60 object-cover" />
+      <h2 className="text-black text-2xl font-bold mt-2">{item_name}</h2>
+      <p className="text-gray-600 text-wrap mt-2">{description}</p>
+      
+      
+          <span
+          className={'border border-gray-300 absolute bg-white  top-8 left-7 min-w-20 max-h-10 text-[2] px-2 py-1 rounded-lg flex justify-center items-center gap-1 text-sm'}  
+        >
+          <BadgeInfo className={iconColorClass} size={16}/>
+          <div className={iconColorClass}>{condition}</div>
+        </span>
+      
+
+        <button
+          onClick={onClick}
+          className="mt-5 fixed bottom-4 right-4 px-4 py-2 text-white bg-orange-300 hover:bg-orange-500 rounded-lg transition"
+        >
+          Veja Mais
+        </button>
+        
+      </div>
     </div>
   );
 }
-
-export default Card;
---
+++ src/components/ItemModal.jsx
@@ -0,0 +1,52 @@
+import ReactDOM from 'react-dom';
+
+export default function ItemModal({ isOpen, onClose, title, children }) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    // Overlay (fundo escuro)
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center transition-colors
+      ${ sOpen ? 'visible bg-black/20' : 'invisible '}"
+      onClick={onClose}
+      onKeyDown={(e) => {
+        if (e.key === 'Escape') {
+          e.stopPropagation();
+        }
+      }}
+      role="button"
+      tabIndex={0}
+    >
+      {/* Container do Modal (para o conteúdo não fechar ao ser clicado) */}
+      <div
+        className=" relative max-h-[90vh] w-full max-w-xs overflow-auto rounded-2xl bg-white p-6 text-black shadow-2xl md:max-w-lg"
+        onClick={(e) => e.stopPropagation()}
+        onKeyDown={(e) => {
+          e.stopPropagation();
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        {/* Cabeçalho com Título e Botão de Fechar */}
+
+        <div className="mb-4 flex items-center justify-between pb-3">
+          <h2 className=" w-full border-indigo-600 border-b-4 pb-3 text-center font-bold text-2xl text-indigo-600">
+            {title}
+          </h2>
+          <button
+            className="font-bold text-2xl text-black"
+            onClick={onClose}
+            type="button"
+          >
+            &times;
+          </button>
+        </div>
+        <h1>TESTANDO</h1>
+        <div>{children}</div>
+      </div>
+    </div>,
+    document.getElementById('portal-root') // Certifique-se de ter <div id="portal-root"></div> no seu index.html
+  );
+}
